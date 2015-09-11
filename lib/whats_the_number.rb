04334432fdd8d0af9943f718ad8257c2dfad83cf
lewis <ljones140@gmail.com>
2015-09-11 13:49:41 +0100

    done

--- lib/whats_the_number.rb
+++ lib/whats_the_number.rb
@@ -1,6 +1,25 @@
-class WhatsTheNumber
-  
-  def self.sum_primes_less_than number
-  end
+require 'prime'
 
+class WhatsTheNumber
+
+  def self.sum_primes_less_than number
+    result = 0
+    Prime.each(number) do |prime|
+      result += prime
+    end
+    result
+  end
 end
+
+
+
+
+
+  # def self.sum_primes_less_than number
+  #   numarr = (0..number).to_a
+  #   numarr.inject(0) do |sum, x|
+  #     if Prime.prime?(x)
+  #      sum += x
+  #    end
+  #   end
+  # end
--- spec/problem_spec.rb
+++ spec/problem_spec.rb
@@ -3,7 +3,7 @@ require './lib/whats_the_number'
 describe WhatsTheNumber do
 
   it 'sums all prime numbers less than 10,000' do
-    expect(WhatsTheNumber.sum_primes_less_than(10000)).to eq 454396537
+    expect(WhatsTheNumber.sum_primes_less_than(10000)).to eq 5736396
   end
 
 end
